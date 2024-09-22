391763460e0e6a8d579bf79ceb6d1bd618793433
ciguliaz <ckazzgd@gmail.com>
2024-09-22 15:19:01 +0700

    build fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,6 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
 
 //    implementation(libs.androidx.activity)
-
 //    implementation(libs.material)
 
 
--- app/src/main/java/com/example/ptitlib/ChangingPasswordActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.ptitlib;
-
-import android.app.Activity;
-
-public class ChangingPasswordActivity extends Activity {
-}
