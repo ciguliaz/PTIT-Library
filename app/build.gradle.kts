44b7068388da3aef5b8972a51f7c03b8dc177989
ciguliaz <ckazzgd@gmail.com>
2024-09-23 12:16:26 +0700

    build fix + some layout fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,11 +61,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.appcompat)
-    implementation("com.google.android.material:material:1.12.0")
-
-//    implementation(libs.androidx.activity)
-//    implementation(libs.material)
-
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
 
     testImplementation(libs.junit)
 
